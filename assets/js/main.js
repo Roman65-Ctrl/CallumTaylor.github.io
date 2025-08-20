b314f39f73b49ba21fd9295f905aa3feaeb1eb69
Roman65-Ctrl <ctaylor5214@gmail.com>
2025-08-20 20:09:31 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
 
+document.querySelectorAll('.fade-section').forEach(section => {
+  observer.observe(section);
+});
